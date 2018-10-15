c7824b61a7ad5a53e64786fbea697c66b5c72e67
Yuki Koyama <yuki@koyama.xyz>
2018-10-15 22:24:27 +0900

    Change API and support constrained, bound/unbound optimization problems (which resolves #4)

--- include/nlopt-util.hpp
+++ include/nlopt-util.hpp
@@ -32,27 +32,42 @@
 
 namespace nloptutil
 {
-    inline Eigen::VectorXd compute(const Eigen::VectorXd& x_initial,
-                                   const Eigen::VectorXd& upper,
-                                   const Eigen::VectorXd& lower,
-                                   const nlopt::vfunc     objective_function,
-                                   void*            data               = nullptr,
-                                   nlopt::algorithm algorithm          = nlopt::LD_TNEWTON,
-                                   int              max_evaluations    = 1000,
-                                   double           relative_func_tol  = 1e-06,
-                                   double           relative_param_tol = 1e-06,
-                                   bool             is_maximization    = false,
-                                   bool             verbose            = false,
-                                   double           initial_step_scale = 1.0)
+    namespace internal
+    {
+        constexpr double constraint_tol = 1e-10;
+    }
+    
+    inline Eigen::VectorXd solve(const Eigen::VectorXd& x_initial,
+                                 const Eigen::VectorXd& upper,
+                                 const Eigen::VectorXd& lower,
+                                 const nlopt::vfunc               objective_function,
+                                 const std::vector<nlopt::vfunc>& equality_constraints,
+                                 const std::vector<nlopt::vfunc>& inequality_constraints,
+                                 nlopt::algorithm algorithm           = nlopt::LD_TNEWTON,
+                                 void*            data                = nullptr,
+                                 bool             is_maximization     = false,
+                                 int              max_evaluations     = 1000,
+                                 double           relative_func_tol   = 1e-06,
+                                 double           relative_param_tol  = 1e-06,
+                                 bool             verbose             = false,
+                                 double           initial_step_scale  = 1.0)
     {
         const unsigned M = static_cast<unsigned>(x_initial.rows());
         
-        const std::vector<double> l(lower.data(), lower.data() + lower.rows());
-        const std::vector<double> u(upper.data(), upper.data() + upper.rows());
-        
         nlopt::opt solver(algorithm, M);
-        solver.set_lower_bounds(l);
-        solver.set_upper_bounds(u);
+        
+        if (upper.rows() != 0)
+        {
+            const std::vector<double> u(upper.data(), upper.data() + upper.rows());
+            solver.set_upper_bounds(u);
+        }
+        
+        if (lower.rows() != 0)
+        {
+            const std::vector<double> l(lower.data(), lower.data() + lower.rows());
+            solver.set_lower_bounds(l);
+        }
+        
         solver.set_maxeval(max_evaluations);
         solver.set_ftol_rel(relative_func_tol);
         solver.set_xtol_rel(relative_param_tol);
@@ -66,6 +81,16 @@ namespace nloptutil
             solver.set_min_objective(objective_function, data);
         }
         
+        for (auto func : equality_constraints)
+        {
+            solver.add_equality_constraint(func, data, internal::constraint_tol);
+        }
+        
+        for (auto func : inequality_constraints)
+        {
+            solver.add_inequality_constraint(func, data, internal::constraint_tol);
+        }
+        
         std::vector<double> x_star(x_initial.data(), x_initial.data() + x_initial.rows());
         
         // Record the cost value for the initial solution
@@ -77,9 +102,13 @@ namespace nloptutil
         }
         
         // Scale the initial step size (only for derivative-free algorithms such as nlopt::LN_COBYLA)
-        std::vector<double> step(M, 0.0);
-        solver.get_initial_step(x_star, step);
-        for (auto& d : step) d *= initial_step_scale;
+        const std::vector<double> step = [&]()
+        {
+            std::vector<double> step(M, 0.0);
+            solver.get_initial_step(x_star, step);
+            for (auto& d : step) { d *= initial_step_scale; }
+            return step;
+        }();
         solver.set_initial_step(step);
         
         // Start timing measurement
@@ -122,6 +151,35 @@ namespace nloptutil
         }
         
         return Eigen::Map<Eigen::VectorXd>(&x_star[0], M);
+    }
+    
+    inline Eigen::VectorXd solve(const Eigen::VectorXd& x_initial,
+                                 const Eigen::VectorXd& upper,
+                                 const Eigen::VectorXd& lower,
+                                 const nlopt::vfunc     objective_function,
+                                 nlopt::algorithm algorithm           = nlopt::LD_TNEWTON,
+                                 void*            data                = nullptr,
+                                 bool             is_maximization     = false,
+                                 int              max_evaluations     = 1000,
+                                 double           relative_func_tol   = 1e-06,
+                                 double           relative_param_tol  = 1e-06,
+                                 bool             verbose             = false,
+                                 double           initial_step_scale  = 1.0)
+    {
+        return solve(x_initial,
+                     upper,
+                     lower,
+                     objective_function,
+                     {},
+                     {},
+                     algorithm,
+                     data,
+                     is_maximization,
+                     max_evaluations,
+                     relative_func_tol,
+                     relative_param_tol,
+                     verbose,
+                     initial_step_scale);
     }
 }
 
